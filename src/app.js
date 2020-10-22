5b5e103c9aa68c3fe950607d81e69fc102a2c05f
Dominique Vilain <dominique.vilain@hepl.be>
2020-10-22 18:09:55 +0200

    cleaning a bit

--- src/app.js
+++ src/app.js
@@ -2,24 +2,31 @@ import 'alpinejs'
 import store from './store'
 
 window.data = function () {
+
 	return {
+
 		...store,
 		filter: 'all',
 		todos: [],
 		todoEditing: null,
 		newTodoTitle: '',
+
 		get activeTodos () {
 			return this.todos.filter(todo => !todo.completed)
 		},
+
 		get activeTodosCount () {
 			return this.activeTodos.length
 		},
+
 		get completedTodos () {
 			return this.todos.filter(todo => todo.completed)
 		},
+
 		get completedTodosCount () {
 			return this.completedTodos.length
 		},
+
 		get filteredTodos () {
 			return {
 				all: this.todos,
@@ -27,13 +34,18 @@ window.data = function () {
 				completed: this.completedTodos
 			}[this.filter]
 		},
+
 		get allTodosComplete () {
 			return this.completedTodosCount === this.todos.length
 		},
+
+
+
 		loadTodos () {
 			let todos = this.fetch()
 			todos.forEach(todo => this.todos.push(todo))
 		},
+
 		addTodo () {
 			if (this.newTodoTitle.trim()) {
 				this.todos.push({
@@ -45,14 +57,17 @@ window.data = function () {
 			}
 			this.save(this.todos)
 		},
+
 		removeTodo (todo) {
 			this.todos.splice(this.todos.indexOf(todo), 1)
 			this.save(this.todos)
 		},
+
 		toggleCompleted (todo) {
 			todo.completed = !todo.completed
 			this.save(this.todos)
 		},
+
 		startEditing (todo, tick) {
 			todo.cachedTitle = todo.title
 			if (this.todoEditing) {
@@ -63,6 +78,7 @@ window.data = function () {
 
 			tick(() => document.getElementById(todo.id).focus())
 		},
+
 		validateEditingTodo (todo) {
 			if (todo.title.trim()) {
 				delete todo.editing
@@ -71,11 +87,13 @@ window.data = function () {
 			}
 			this.save(this.todos)
 		},
+
 		cancelEditingTodo (todo) {
 			todo.title = todo.cachedTitle
 			delete todo.cachedTitle
 			delete todo.editing
 		},
+
 		toggleAllTodosComplete () {
 			let allTodosComplete = this.allTodosComplete
 			this.todos.forEach(todo => todo.completed = !allTodosComplete)
--- src/store.js
+++ src/store.js
@@ -1,11 +1,15 @@
 const store = {
+
 	ls: window.localStorage,
+
 	save (todos) {
 		this.ls.setItem('todos', JSON.stringify(todos))
 	},
+
 	fetch () {
 		return JSON.parse(this.ls.getItem('todos'))
 	}
+
 }
 
 export default store
