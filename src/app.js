61d6a1635e220bc49733d9cfa5399899af6861bb
Dominique Vilain <dominique.vilain@hepl.be>
2020-10-22 10:35:34 +0200

    add addTodo basic functionnality

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ import 'alpinejs'
 
 window.data = function () {
 	return {
-		todos: []
+		todos: [],
+		newTodoTitle: null,
+		addTodo () {
+			this.todos.push({
+				id: Date.now(),
+				title: this.newTodoTitle,
+				completed: false
+			})
+			console.log(this.todos)
+		}
 	}
 }
